9d219e239034d9678db41f0fa62ea78786fcd6fa
Sven Vidak <sven.vidak@gmail.com>
2020-04-10 03:04:12 +0200

    Remove unused datetime methods

--- internal/datetime/datetime.go
+++ internal/datetime/datetime.go
@@ -12,32 +12,12 @@ func Now() time.Time {
 	return time.Now()
 }
 
-func NowFormatted() string {
-	return time.Now().Format(yyyyMmDd)
-}
-
 func WeekStart() time.Time {
 	return now.Monday()
 }
 
 func WeekEnd() time.Time {
 	return now.Sunday().AddDate(0, 0, -2)
-}
-
-func MonthStartFormatted() string {
-	return now.BeginningOfMonth().Format(yyyyMmDd)
-}
-
-func MonthEndFormatted() string {
-	return now.EndOfMonth().Format(yyyyMmDd)
-}
-
-func YearStartFormatted() string {
-	return now.BeginningOfYear().Format(yyyyMmDd)
-}
-
-func YearEndFormatted() string {
-	return now.EndOfYear().Format(yyyyMmDd)
 }
 
 func ToISODate(dateString string) time.Time {
